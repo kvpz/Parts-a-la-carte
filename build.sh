714ad33a6dc9d30f184822e5dade2fadde441e0f
Kevin Perez <unknotted26@gmail.com>
2017-10-30 18:33:20 -0400

    request_shopjimmy ready for cgi application.

--- build.sh
+++ build.sh
@@ -1,18 +1,24 @@
 
 MYHTML_HEADERS=/usr/local/include/myhtml
 LIB=/usr/local/lib
+LIBHTML=/usr/local/lib/myhtml_static
 
 echo 'gcc -g -I/usr/local/include/myhtml -L/usr/local/lib get_product_names.c -o get_product_names -lmyhtml'
 gcc -g -I$MYHTML_HEADERS -L$LIB get_product_names.c -o get_product_names -lmyhtml 
+#gcc -g -static -L/usr/lib64 -L$LIB -lc -lmyhtml_static -I$MYHTML_HEADERS get_product_names.c -o get_product_names 
 
 echo 'gcc -g -I$MYHTML_HEADERS -L$LIB driver.c -o driver -lmyhtml -lcurl'
-gcc -g -I$MYHTML_HEADERS -L$LIB api.c driver.c -o driver -lmyhtml -lcurl
+#gcc -g -I$MYHTML_HEADERS -L$LIB api.c driver.c -o driver -lmyhtml -lcurl
+#gcc -g $LIBHTML -I$MYHTML_HEADERS api.c driver.c -o driver -lcurl
+
+echo 'gcc -g -I$MYHTML_HEADERS -L$LIB request_shopjimmy.c -o request_shopjimmy -lmyhtml -lcurl'
+gcc -g -I$MYHTML_HEADERS -L$LIB request_shopjimmy.c -o request_shopjimmy -lmyhtml -lcurl
 
 # If executable emits an error related to shared libraries, make sure
 # the environment variable $LD_LIBRARY_PATH is set correctly.
 # The $LD_LIBRARY_PATH variable should be set to the location of the
-# shared libraries: export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH.
-
+# libraries: export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+# in order to make it available at runtime.
 
 # RESOURCE
 # https://www.cprogramming.com/tutorial/shared-libraries-linux-gcc.html
--- request_shopjimmy.c
+++ request_shopjimmy.c
@@ -2,10 +2,19 @@
   Requests for Shop Jimmy
   
   This program is designed to make requests to Shop Jimmy using
-  libcurl.
-  
+  libcurl. The request is logged and the response is printed to a
+  file identified as <time>_sj.
 
-  10/17/2017
+  The program is not always be trusted when it mentions that a file
+  is cached because it relies on the log file and not necessarily if 
+  the file actually exists. 
+
+  The program should only print one line to stdout during production. 
+  This line of text will represent the location where the response body
+  was saved. Note that the location the file is saved should be accessible
+  by the http server for reading and writing.
+
+  Updated 10/29/2017
   Written by Kevin Perez
 */
 #include <stdio.h>
@@ -17,6 +26,10 @@
 
 #define shopjimmy_uri "http://www.shopjimmy.com/catalogsearch/result/?q="
 #define REQUEST_CACHED 1
+//#define FILE_LOCATION "/home/kperez/Documents/TV_Parts_and_More/"
+//#define FILE_LOCATION "/var/www/html/~kp/"
+#define FILE_LOCATION "/usr/tmp/"
+
 /*
   A single log's format: 
   <time> <query> <provider> <response file> \n
@@ -31,7 +44,7 @@
   Complexity: high
  */
 int log_request(const char* query_string, const char* time,
-		const char* provider, const char* response_file)
+		const char* provider, char* response_file)
 {
   int status = 0;
   FILE* log = fopen("request_log", "r+");
@@ -53,6 +66,8 @@ int log_request(const char* query_string, const char* time,
       if(strcmp(log_query, query_string) == 0){
 	// the request has been made and shouldn't be repeated.
 	fclose(log);
+	//printf("Request (cached) saved to: %s_js\n", time_str);
+	sprintf(response_file, "%s_sj", time_str);
 	free(time_str);
 	return REQUEST_CACHED;
       }
@@ -61,8 +76,9 @@ int log_request(const char* query_string, const char* time,
   
   // Write to log
   fgetc(log);
-  fprintf(log, "%s %s %s %s\n", time, query_string, provider,
+  fprintf(log, "%s %s %s /usr/tmp/%s\n", time, query_string, provider,
 	  response_file);
+  printf("%s", response_file);
   
   fclose(log);
   free(time_str);
@@ -76,57 +92,84 @@ static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
 }
 
 int main(int argc, char* argv[])
-{
-  
-  if(argc != 2){
+{  
+  if(argc <= 1 || argc > 3){
     printf("The search term should be provided as an argument.\n");
     return -1;
   }
   
-  CURL* curl;
+  char* save_file_location;
+  if(argc == 3){
+    save_file_location = malloc(sizeof(char) * strlen(argv[2]));
+    save_file_location = argv[2];
+  }
 
   time_t timer = time(NULL);
   time(&timer);
   char time_str[11];
   sprintf(time_str, "%d", timer);
-
   char* response_file = malloc(30);
-  sprintf(response_file, "%s_%s", time_str, "sj");
 
-  log_request(argv[1], time_str, "SJ", response_file);
+  int status = log_request(argv[1], time_str, "SJ", response_file);
+  if(status == REQUEST_CACHED){
+    //printf("The request has already been made within the last 12 hours.\n");
+    printf("%s", response_file);
+    return 0;
+  }
 
-  char body_filename[19];
-  sprintf(body_filename, "%s_%s", time_str, response_file);
-  FILE* bodyFile;
+  sprintf(response_file, "%s_sj", time_str);
+  FILE* outfile;
 
   char* request_uri = malloc(strlen(shopjimmy_uri) + strlen(argv[1]));
   sprintf(request_uri, "http://www.shopjimmy.com/catalogsearch/result/?q=%s",
-  argv[1]);
+	  argv[1]);
   
-  printf("The requested uri is: %s\n", request_uri);
+  //printf("The requested uri is: %s\n", request_uri);
   
-  curl_global_init(CURL_GLOBAL_ALL);
-  curl = curl_easy_init();
-  curl_easy_setopt(curl, CURLOPT_URL, request_uri);
-  curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-
-  // Open file to write response body
-  bodyFile = fopen(body_filename, "wb");
-  if(!bodyFile){
-    curl_easy_cleanup(curl);
+  if(curl_global_init(CURL_GLOBAL_ALL) != 0){
+    fprintf(stderr, "There is an issue with curl. Global init error.\n");
     return -1;
   }
 
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, bodyFile);
+  CURL* curl;
+  curl = curl_easy_init();
+  if(curl == 0){
+    fprintf(stderr, "curl_easy_init() error\n");
+    return -1;
+  }
+  struct curl_slist* slist = NULL;
+  slist = curl_slist_append(slist, "Accept: text/html, application/xhtml+xml");
+  slist = curl_slist_append(slist, "Accept-Language: en-US,en;q=0.5");
+  slist = curl_slist_append(slist, "DNT: 1");
+  curl_easy_setopt(curl, CURLOPT_URL, request_uri);
+  curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+  
+  char* response_file_location =
+    malloc(strlen(FILE_LOCATION) + strlen(response_file)+1);
+  sprintf(response_file_location, "%s%s", FILE_LOCATION, response_file);
+  
+  // Open file to write response body
+  outfile = fopen(response_file_location, "wb");
+  if(!outfile){
+    printf("opening file failed\n");
+    fprintf(stderr, "Opening file %s failed\n", response_file_location);
+    curl_easy_cleanup(curl);
+    return -1;
+  }
+  printf("%s", response_file_location);
+  
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, outfile);
 
-  //curl_easy_perform(curl);
+  curl_easy_perform(curl);
 
-  fclose(bodyFile);
+  fclose(outfile);
 
   curl_easy_cleanup(curl);
 
   free(request_uri);
+  free(response_file);
+  free(response_file_location);
   
   return 0;
 }
